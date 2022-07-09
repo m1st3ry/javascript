d4b1c32d1b4835719c9a6ce2add66edadbbd7a9b
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-09 19:51:33 -0300

    Objetos
    Entendendo objeto no console do navegador.

---
+++ Introdução/Valores_e_tipos/objetos.js
@@ -0,0 +1,14 @@
+// Objetos
+
+var aluno1 = {
+    matricula: 764758,
+    nome: 'Robinson Vatam',
+    curso: 'Bootcamp Front End',
+    ativo: true
+};
+
+console.log('aluno1.nome');
+
+aluno1.dataNascimento = '13/04/2010'
+
+console.log('aluno1.dataNascimento');
