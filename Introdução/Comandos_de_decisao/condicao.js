9bf83323f0b939b7b6108fb7ea3b02e409105aa3
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 20:21:12 -0300

    comandos de condição
    exemplo com else if e switch.

--- Introdução/Comandos_de_decisao/condicao.js
+++ Introdução/Comandos_de_decisao/condicao.js
@@ -14,3 +14,32 @@ function menor(a, b, c) {
     var r = a < b && a < c ? a : b < a && b < c ? b : c;
     return r;
 }
+
+function formataMes(mes) {
+    switch (mes) {
+        case 1:
+            return "Janeiro";
+        case 2:
+            return "Fevereiro";
+        case 3:
+            return "Março";
+        case 4:
+            return "Abril";
+        case 5:
+            return "Maio";
+        case 6:
+            return "Junho";
+        case 7:
+            return "Julho";
+        case 8:
+            return "Agosto";
+        case 9:
+            return "Setembro";
+        case 10:
+            return "Outubro";
+        case 11:
+            return "Novembro";
+        default:
+            return "Não terminado!";
+    }
+}
