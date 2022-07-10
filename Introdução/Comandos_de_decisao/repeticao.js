ba32f614546279d14755f089bd722a942ab3f896
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 20:48:28 -0300

    brekando nosso for
    utilizamos break para parar nosso contador no 3.

--- Introdução/Comandos_de_decisao/repeticao.js
+++ Introdução/Comandos_de_decisao/repeticao.js
@@ -13,3 +13,12 @@ do {
     console.log(contador2);
     contador2++;
 }   while(contador2 <= 5);
+
+var contador3 = 1
+console.log('Imprimindo de 1 a 5 com for');
+for (; contador3 <= 5; contador3++){
+    console.log(contador3);
+    if (contador3 === 3) {
+        break;
+    }
+}
