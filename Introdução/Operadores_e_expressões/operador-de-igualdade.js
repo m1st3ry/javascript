eb910a5e9a3b373b7466f92b12d81e257710fc50
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 12:29:58 -0300

    Operadores de igualdade

---
+++ Introdução/Operadores_e_expressões/operador-de-igualdade.js
@@ -0,0 +1,22 @@
+// realizar os teste no console do navegador.
+
+// x == y Igual
+// exemplo:
+// 5 == 5 = true
+// 5 == '5' = true // Isso ocorre porque muitas vezes o javascript realiza conversão se necessario.
+
+// nem sempre isso fica muito claro ou preciso
+// false == 0 = true
+// false == '' = true
+// '' == 0 = true
+
+// Por isso o adequado é usar para comparar valores.
+// 5 === 5 = false
+// 5 !== '5' = true
+
+
+// x != y Diferente
+
+// x === y Exatamente igual
+
+// !== y Não exatamente
