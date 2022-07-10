6276f52f110689ff73e53b3d7e3ec4d5e5a0025b
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 13:54:10 -0300

    funções
    Entendendo variaveis globais, escopo e declaração de funções.

--- Introdução/Funcoes_e_escopos/praticando-funcoes.js
+++ Introdução/Funcoes_e_escopos/praticando-funcoes.js
@@ -1,4 +1,26 @@
+var m = 'Olá';
+imprimeOla();
+
+function imprimeOla(){
+    var nome = 'Robinson';
+    console.log(m);
+    console.log(nome);
+    imprimeOla2();
+
+    function imprimeOla2() {
+        console.log(m);
+        console.log(nome);
+    }
+}
+
 function soma(op1, op2) {
     return op1 + op2;
 }
 
+function dados() {
+    var nome = 'Robinson';
+    var idade = 39;
+    var cidade = 'São Paulo';
+    var CEP = '05150-000';
+    var CPF = '313848343-09';
+}
