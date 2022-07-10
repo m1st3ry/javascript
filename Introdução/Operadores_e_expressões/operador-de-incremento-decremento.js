fb5e3f1ec01765d6aed33706c28546f59eae9c04
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 12:19:55 -0300

    operadores de incremento e decremento

---
+++ Introdução/Operadores_e_expressões/operador-de-incremento-decremento.js
@@ -0,0 +1,9 @@
+// Realizar os testes no console do navegador.
+
+// x++  retorna x e depois incrementa
+
+// x-- retorna x e depois decrementa
+
+// ++x Incrementa e depois retorna x
+
+// --x Decrementa e depois retorna x
