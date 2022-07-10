4a130ebb011ac1c6e675ef3d10bef1b41fcaa7d0
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-09 21:23:04 -0300

    Operadores
    Aprendendo boleano em javascript.

---
+++ Introdução/Operadores_e_expressões/boleano.js
@@ -0,0 +1,26 @@
+// Negação
+
+!true = false;
+!false = true;
+
+// And ou E 
+
+true && false = false;
+
+true && true = true;
+
+// Ou
+
+false || true = true;
+
+false || flase = false;
+
+4 || 0 = 4;
+
+0 || 4 = 4;
+
+7 || 4 = 7;
+
+7 && 4 = 4;
+
+7 && 0 = 0;
