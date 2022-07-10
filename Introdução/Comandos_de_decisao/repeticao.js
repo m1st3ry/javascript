c410dd832a6cd1355df06a062e82650d4a2f5d8f
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 20:37:35 -0300

    cirando outra repetição com do while

--- Introdução/Comandos_de_decisao/repeticao.js
+++ Introdução/Comandos_de_decisao/repeticao.js
@@ -1,7 +1,15 @@
-console.log('imprimindo de 1 a 5 contador');
+console.log('imprimindo de 1 a 5 com While');
 
 var contador1 = 1;
 while(contador1 <= 5) {
     console.log(contador1);
     contador1++;
 }
+
+console.log('Imprimindo de 1 a 5 com do While');
+
+var contador2 = 1;
+do {
+    console.log(contador2);
+    contador2++;
+}   while(contador2 <= 5);
