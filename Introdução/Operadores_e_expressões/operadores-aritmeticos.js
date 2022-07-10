3b13cdaf9140dcbdef76cf9a5ef35f56391afcdc
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 12:01:49 -0300

    Operadores Aritmeticos

---
+++ Introdução/Operadores_e_expressões/operadores-aritmeticos.js
@@ -0,0 +1,42 @@
+// Realize os teste no console do navegador.
+
+// x + y  Adição
+// exemplo:
+// 1 + 1 = 2
+
+
+// x - y Subtração
+// exemplo:
+// 2 - 1 = 1
+
+
+// +x  Converte para número
+// +'24' = 24 
+// Como as aspas definem uma string, ele converte ela para número.
+
+
+// -x  Inversão de sinal
+// exemplo:
+// -2 = -2
+// -+2 = -2
+
+
+// x / y Divisao
+// exemplo: 
+// 6 / 2 = 3
+
+
+// x * y Multiplicação
+// exemplo:
+// 2 * 2 = 4
+
+
+// x % y Resto da divisão de x por y
+// exemplo:
+// 7%2 = 1
+// 11%7 = 4
+
+
+// x ** y Exponenciação 
+// exemplo:
+// 2 **3 = 8
