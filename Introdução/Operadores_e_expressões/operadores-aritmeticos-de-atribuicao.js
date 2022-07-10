355bc7c299fb54d1a2f7cf9468254a2f4a2038a4
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 12:10:51 -0300

    operadores aritméticos de atribuição
    operadores aritméticos de atribuição e 1 exemplo de uso.

---
+++ Introdução/Operadores_e_expressões/operadores-aritmeticos-de-atribuicao.js
@@ -0,0 +1,28 @@
+//Realizar o teste no console do navegador.
+
+// x += y  Adição
+
+// x -= y  Subtração
+
+// x /= y Divisão
+
+// x *= y Multiplicação
+
+// x %= y Resto da divisão de x por y
+
+// x **= y Exponenciação
+
+
+//exemplo:
+
+//var x = 5;
+// resulta indefined
+
+// x += 2;
+// resulta 7 // Ele somou 2 a variavel 5 resultando em 7
+
+// x
+// 7
+
+// x **= 2
+// 49 // pegou o valor da variavel, 7 e elevou a 2, resultando em 49
