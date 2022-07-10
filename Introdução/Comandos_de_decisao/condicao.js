8a7cb7c92cc74f9f19b89bdcb0f9979bd592016a
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-10 20:09:11 -0300

    exemplos de condição
    Bem, por algum motivo o arquivo index.html na pasta de condição não esta funcionando, então continuo os exercicios linkando em outro html para testar as funções.

--- Introdução/Comandos_de_decisao/condicao.js
+++ Introdução/Comandos_de_decisao/condicao.js
@@ -1,10 +1,16 @@
-console.log(maior);
 
-function maior(a, b) {
-    if (a > b) {
+
+function maior(a, b, c) {
+    if (a > b && a > c) {
         return a;
+    }   else if (b > a && b > c) {
+        return b;
+    }   else {
+        return c;
     }   
-    return b;
-    
 }
 
+function menor(a, b, c) {
+    var r = a < b && a < c ? a : b < a && b < c ? b : c;
+    return r;
+}
