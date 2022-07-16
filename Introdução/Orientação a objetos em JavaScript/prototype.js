1f685b7e550c29f576eeea1194cb11b953938f11
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-15 23:18:25 -0300

    Finalizando aula

--- Introdução/Orientação a objetos em JavaScript/prototype.js
+++ Introdução/Orientação a objetos em JavaScript/prototype.js
@@ -5,8 +5,19 @@ function Retangulo(altura, largura) {
     this.largura = largura;
     this.area = function() {
         return this.largura * this.altura;
-    }
+    };
 }
 
 var r1 = new Retangulo(3, 4);
 var r2 = new Retangulo(7, 2);
+
+function Retangulo(altura, largura) {
+    this.altura = altura;
+    this.largura = largura;
+}
+RetanguloV2.prototype.area = function () {
+    return this.largura * this.altura;
+};
+
+var r3 = new RetanguloV2(3, 4);
+var r4 = new RetanguloV2((7, 2);
