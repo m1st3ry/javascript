525600e275c02e0ded075c4f321f87a54dec250e
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-15 23:11:54 -0300

    usando Prototype chain

---
+++ Introdução/Orientação a objetos em JavaScript/prototype.js
@@ -0,0 +1,9 @@
+//Testes feitos no console do navegador.
+
+function Retangulo(altura, largura) {
+    this.altura = altura;
+    this.largura = largura;
+    this.area = function() {
+        return this.largura * this.altura;
+    }
+}
