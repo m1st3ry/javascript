0c29aa8dec9418bb2b5f7231d8c42ca38c8ebfc7
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-15 23:10:25 -0300

    arrumando um erro no codigo
    Esqueci de colocar o fechamento da primeira função.

--- Introdução/Orientação a objetos em JavaScript/instanciando.js
+++ Introdução/Orientação a objetos em JavaScript/instanciando.js
@@ -1,8 +1,9 @@
 //Testes feitos no console do navegador.
 
-function Retangulo(altura, largura) {
-    this.altura = altura;
-    this.largura = largura;
-    this.area = function() {
-        return this.largura * this.altura;
-}
+    function Retangulo(altura, largura) {
+        this.altura = altura;
+        this.largura = largura;
+        this.area = function() {
+            return this.largura * this.altura;
+        }
+    }
