0ff344e615f053df312a8b27f3bfba595e30d869
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-17 23:15:47 -0300

    Spread Operator

---
+++ Introdução/Javascript_Funcional/spread_op.js
@@ -0,0 +1,10 @@
+// testando no console do navegador.
+
+let primo = [2, 3, 5, 7, 11, 13];
+
+let curso = {
+    nome: "Bootcamp Front End",
+    modulos: 4,
+    presencial: false,
+    turma: 1,
+};
