bc6c54a9ea7d5373f125c328ad51151e356c5569
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-17 23:37:35 -0300

    update

--- Introdução/Javascript_Funcional/arrowFunc.js
+++ Introdução/Javascript_Funcional/arrowFunc.js
@@ -1,3 +1,16 @@
 // testando no console do navegador.
 
 var somaV2 = (a, b) => a + b;
+
+function RetanguloV2(altura, largura) {
+    this.altura = altura;
+    this.largura = largura;
+    this.area = () => this.altura * this.largura;
+}
+
+var r1 = new RetanguloV2(5, 6);
+
+var imprimeMensagem = m => console.log(m);
+
+var imprimeMensagem2 = () => console.log("teste");
+// é necessario declarar o parametro para usar arrow functions assim.
