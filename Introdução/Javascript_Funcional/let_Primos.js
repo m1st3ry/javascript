cb66c19503f1b4ac20cfebe019c4da5b6fe12c74
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-17 23:12:53 -0300

    update

--- Introdução/Javascript_Funcional/let_Primos.js
+++ Introdução/Javascript_Funcional/let_Primos.js
@@ -8,3 +8,11 @@ let curso = {
     presencial: false,
     turma: 1
 }
+
+let [p1, p2, ...resto] = primos;
+
+let {nome: nomeCurso, turma, ...outrosCampos} = curso;
+
+function imprime({ nome }) {
+    console.log(nome);
+}
