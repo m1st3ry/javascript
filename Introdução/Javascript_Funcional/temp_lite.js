5f3e32182e3d91b4faa8b92d1ec9dd1be1991e0e
Robinson Vatam <robinson.vatam@protonmail.com>
2022-07-17 23:24:55 -0300

    template literals

---
+++ Introdução/Javascript_Funcional/temp_lite.js
@@ -0,0 +1,9 @@
+// testando n console do navegador.
+
+let a = 2, b = 3;
+let soma = a + b;
+
+console.log(a + " + " + b + " = " + soma);
+
+console.log(`${a} + ${b} = ${soma}`);
+//Podemos declarar da mesma forma, usando " ` " crase e sifrão.
